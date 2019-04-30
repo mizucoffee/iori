204d162091f148bbc148a0f506810fbd1c27b971
Ritsuki Goto <develop@mizucoffee.net>
2019-05-01 01:21:13 +0900

    [fix] travis

--- db/schema.rb
+++ db/schema.rb
@@ -13,9 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20190428065923) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "artists", force: :cascade do |t|
     t.string "name"
   end
