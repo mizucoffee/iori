bf03306fc05827a51db032d5a2bbbd6808a90288
Ritsuki Goto <develop@mizucoffee.net>
2019-05-05 02:42:01 +0900

    [add] music page

--- src/routes/music.rb
+++ src/routes/music.rb
@@ -2,8 +2,13 @@
 class MusicRouter < Base
   get '/' do
     @q = params[:q]
-    pp params[:q]
-    @musics = @q.blank? ? [] : Music.where('name like ?', "%#{params[:q]}%")
+    @musics = @q.blank? ? [] : Music.where('name like ?', "%#{@q}%")
     erb :'routes/music/root'
+  end
+
+  get '/:music_id' do
+    @music = Music.find(params[:music_id])
+    # 404処理等
+    erb :'routes/music/music'
   end
 end
