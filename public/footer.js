e6a9b084f30ab78e48ddad3dadea961335ef3680
Ritsuki Goto <develop@mizucoffee.net>
2019-05-09 02:32:13 +0900

    [fix] footer resize

--- public/footer.js
+++ public/footer.js
@@ -1,4 +1,8 @@
-$(document).ready(() => {
+function adjust() {
+    $('footer').css('margin-top', 0)
     const m = window.innerHeight - $('html').height()
     if (m > 0) $('footer').css('margin-top', m)
-})
+}
+
+$(document).ready(adjust)
+$(window).resize(adjust)
